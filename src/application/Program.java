bcf12c81f910961b75f3e4a725fe62278ba9cf5e
Michel Morais <valentimmichel@gmail.com>
2023-03-28 16:59:54 -0300

    Expressão lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,9 @@
-//Expressão lambda declarada
+//Expressão lambda inline
 package application;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -22,9 +21,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-		
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 
 		names.forEach(System.out::println);
 
