fc2996fe13641c38a0469140f7fb3ebc71668245
Michel Morais <valentimmichel@gmail.com>
2023-03-28 16:50:22 -0300

    Reference method com metódo estático

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,6 @@ import java.util.Locale;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -23,7 +22,7 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		
-	List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+	List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
 	
 	names.forEach(System.out::println);
 		
