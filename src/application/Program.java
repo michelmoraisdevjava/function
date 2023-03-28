ae70b25e62110f5086469b001c31dee82fc1787c
Michel Morais <valentimmichel@gmail.com>
2023-03-28 16:57:58 -0300

    Expressão lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
-//Reference method com metódo não estático
+//Expressão lambda declarada
 package application;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -11,7 +12,7 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 
 		List<Product> list = new ArrayList<>();
@@ -21,10 +22,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
 		
-	List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
-	
-	names.forEach(System.out::println);
-		
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
+
+		names.forEach(System.out::println);
+
 	}
 }
