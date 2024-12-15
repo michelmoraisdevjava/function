1c1c4ab8ead38a8803bd17c9f9ad322dc5248db7
Michel Morais <valentimmichel@gmail.com>
2024-12-15 20:58:30 -0300

    Declared lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,9 @@
-//Reference method with non-static method
+//Declared lambda expression
 package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -17,7 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> names = list.stream().map(Product::nonStaticToUppercaseName).collect(Collectors.toList());
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 
